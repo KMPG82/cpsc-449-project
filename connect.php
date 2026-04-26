e36883be1de88f66ac47cad3f0185f322abdd383
saral1025 <saral1025@csu.fullerton.edu>
2026-04-25 22:29:18 -0700

    Added new lost and found test items and updated database setup

--- connect.php
+++ connect.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "";
+$password = "password123";
 $dbname = "LAF";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
